21320b004cc22d756d936ed556ede1d76d05150f
nafiniyad <96862605+nafiniyad@users.noreply.github.com>
2021-12-30 14:52:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+num = 10 
+row = 0 
+while row < num:
+    space = num-row-1
+    while space > 0:
+        print(" ",end="")
+        space = space-1
+    star = row*2+1
+    while star > 0:
+        print("*",end="")
+        star = star-1
+    row = row+1
+    print()
+print("        [*]  ")
+print("     [*][*][*]")
+print("-------------------")
+print()
